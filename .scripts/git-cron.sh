258d54ca8ea908536ea0e0f87f31efaf41d4d549
Dean-Palmer <dean@spapartsvortex.eu>
2018-10-11 09:11:43 +0100

    message

---
+++ .scripts/git-cron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git add .mutt
+git add .screenlayout
+git add .scripts
+git add .vim
+git add .Xauthority
+git add .Xresources
+git add .bashrc
+git add .vimrc
+git add .xinitrc
+
+git commit
---
+++ .scripts/git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git add ~/.mutt
+git add ~/.screenlayout
+git add ~/.scripts
+git add ~/.vim
+git add ~/.Xauthority
+git add ~/.Xresources
+git add ~/.bashrc
+git add ~/.vimrc
+git add ~/.xinitrc
+
+read message
+
+echo "Commit message"
+
+git commit -m "$message"
